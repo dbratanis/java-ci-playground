ce77c7bad0286cebbe1ae9b17049740d3626660c
dbratanis <dbratanis@gmail.com>
2022-10-14 18:19:28 +0300

    :book:

---
+++ src/main/java/com/db/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.db;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		var result = WelcomeMessage("Welcome to FC");
+		System.out.println(result);
+	}
+
+	public static String WelcomeMessage(String text) {
+		return text + " Barcelona";
+	}
+
+}
