d9e162154e8200c23c4ca2fe1b17346f45c382f9
dbratanis <dbratanis@gmail.com>
2022-10-18 13:34:40 +0300

    Lint code

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,15 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * This module contains all the tests for HelloWorld
+ * <p>
+ * Tests that referring to HelloWorld class
+ * </p>
+ *
+ * @since 0.0.1
+ * @author com.db
+ * @version 0.0.1
+ */
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import com.db.*;
+import com.db.HelloWorld;
 
 class HelloWorldTest {
 
 	@Test
 	@DisplayName("Test WelcomeMessage input")
 	void testPrintMethod() {
-		
+	
 		String inputValue = "FC";
 		String excpectedResult = "FC";
 	
